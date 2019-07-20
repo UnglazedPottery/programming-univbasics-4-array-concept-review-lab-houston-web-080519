80c708e3c3c7cfdeb7ee93ef0fb11b4e354c8cd4
Jade Huynh <35jadeh@gmail.com>
2019-07-20 19:44:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0
   while array[counter] do		
-    puts pets[counter]
+    array[counter] if array[counter] == value_to_find
     counter += 1
   end
 
