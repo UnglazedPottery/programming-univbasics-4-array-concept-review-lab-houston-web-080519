45e6f378d758c5e293d5fe56cb24d9f9931342aa
Jade Huynh <35jadeh@gmail.com>
2019-07-20 19:57:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,12 +4,14 @@ def find_element_index(array, value_to_find)
     return counter if array[counter] == value_to_find
     counter += 1
   end
-
 end
 
 def find_max_value(array)
-  
-
+  counter = 0
+  while array[counter] do		
+    if 
+    counter += 1
+  end
 
 end
 
